a6da6e1c795b2d845559f4cc1f13802595b10b06
davidgg00 <51287306+davidgg00@users.noreply.github.com>
2020-05-04 20:37:56 +0200

    Comparar Jugadores centrado y control de error
    Centramos la vista de comparar jugadores y si no existe las stats de un jugador te muestra "N/A"

--- application/views/compararjugadores_v.php
+++ application/views/compararjugadores_v.php
@@ -3,28 +3,29 @@
     //por cada elemento del array del POST['jugador'] mostramos sus estadisticas medias y totales
     //$estadisticas es un array que dentro tiene un objetos.
     foreach ($datos_user as $n => $jugador) : ?>
-        <div id="estadistica_media" class="w-50">
-            <h3><?php echo $jugador->username ?></h3>
+        <div class="w-50 estadistica_media d-flex justify-content-center flex-wrap">
+            <h3 class="w-100 text-center"><?php echo $jugador->username ?></h3>
             <img src="<?= base_url($jugador->imagen) ?>" class="img-fluid w-25 mx-auto" alt="">
-            <h2 class="text-center">Estadísticas Media</h2>
-            <h4>Triples metidos: <?= $estadisticas[$n]->triples /  $estadisticas[$n]->partidos_jugados ?></h4>
-            <h4>Tiros de 2 metidos: <?= $estadisticas[$n]->tiros_2 /  $estadisticas[$n]->partidos_jugados ?></h4>
-            <h4>Tiros libres metidos: <?= $estadisticas[$n]->tiros_libres /  $estadisticas[$n]->partidos_jugados ?></h4>
-            <h4>Tapones: <?= $estadisticas[$n]->tapones /  $estadisticas[$n]->partidos_jugados ?></h4>
-            <h4>Robos: <?= $estadisticas[$n]->robos /  $estadisticas[$n]->partidos_jugados ?></h4>
+            <h2 class="text-center w-100">Estadísticas Media</h2>
+            <h4 class="w-100 text-center">Triples metidos: <?= ($estadisticas[$n]->triples) ? $estadisticas[$n]->triples /  $estadisticas[$n]->partidos_jugados : "N/A"; ?></h4>
+            <h4 class="w-100 text-center">Tiros de 2 metidos: <?= ($estadisticas[$n]->tiros_2) ? $estadisticas[$n]->tiros_2 /  $estadisticas[$n]->partidos_jugados : "N/A"; ?></h4>
+            <h4 class="w-100 text-center">Tiros libres metidos: <?= ($estadisticas[$n]->tiros_libres) ? $estadisticas[$n]->tiros_libres /  $estadisticas[$n]->partidos_jugados : "N/A"; ?></h4>
+            <h4 class="w-100 text-center">Tapones: <?= ($estadisticas[$n]->tapones) ?  $estadisticas[$n]->tapones /  $estadisticas[$n]->partidos_jugados : "N/A"; ?></h4>
+            <h4 class="w-100 text-center">Robos: <?= ($estadisticas[$n]->robos) ? $estadisticas[$n]->robos /  $estadisticas[$n]->partidos_jugados : "N/A"; ?></h4>
         </div>
     <?php endforeach; ?>
     <?php foreach ($_POST['jugador'] as $n => $jugador) : ?>
-        <div id="estadistica_total" class="w-50">
-            <h2 class="text-center">Estadisticas Totales</h2>
-            <h4>Triples metidos: <?= $estadisticas[$n]->triples ?></h4>
-            <h4>Tiros de 2 metidos: <?= $estadisticas[$n]->tiros_2 ?></h4>
-            <h4>Tiros libres metidos: <?= $estadisticas[$n]->tiros_libres ?></h4>
-            <h4>Tapones: <?= $estadisticas[$n]->tapones ?></h4>
-            <h4>Robos: <?= $estadisticas[$n]->robos ?></h4>
+        <div class="w-50 estadistica_total">
+            <h2 class="text-center w-100">Estadisticas Totales</h2>
+            <h4 class="w-100 text-center">Triples metidos: <?= ($estadisticas[$n]->triples) ? $estadisticas[$n]->triples : "N/A"; ?></h4>
+            <h4 class="w-100 text-center">Tiros de 2 metidos: <?= ($estadisticas[$n]->tiros_2) ? $estadisticas[$n]->tiros_2 : "N/A";  ?></h4>
+            <h4 class="w-100 text-center">Tiros libres metidos: <?= ($estadisticas[$n]->tiros_libres) ? $estadisticas[$n]->tiros_libres : "N/A"; ?></h4>
+            <h4 class="w-100 text-center">Tapones: <?= ($estadisticas[$n]->tapones) ? $estadisticas[$n]->tapones : "N/A"; ?></h4>
+            <h4 class="w-100 text-center">Robos: <?= ($estadisticas[$n]->robos) ? $estadisticas[$n]->robos : "N/A";  ?></h4>
         </div>
     <?php endforeach; ?>
     <?php foreach ($_POST['jugador'] as $n => $jugador) : ?>
+
         <div id="partidos" class="w-50">
             <h2 class="text-center">Estadisticas Partidos</h2>
             <table class="table table-bordered bg-white text-center">
@@ -39,7 +40,8 @@
                     </tr>
                 </thead>
                 <tbody>
-                    <?php foreach ($stats_ind[$n] as $partido) : ?>
+                    <?php foreach ($stats_ind[$n] as $partido) :
+                    ?>
                         <tr>
                             <?php if ($partido->id_local === $_SESSION['equipo']) : ?>
                                 <td><?= $partido->equipo_visitante ?></td>
