5843936a361db8d9d9739826fe0d15cbd7406725
davidgg00 <51287306+davidgg00@users.noreply.github.com>
2020-05-04 20:39:06 +0200

    Mínimo y máximo de comparar jugadores
    La acción de comparar las estadísticas de los jugadores por ahora tiene un mínimo de 2 jugadores y un máximo de 2 (posiblemente sea 4 en un futuro). Si se intenta comprar 0,1 o +2 jugadores lanzará una alerta la página.

--- application/views/listajugadores_v.php
+++ application/views/listajugadores_v.php
@@ -1,28 +1,44 @@
 <script>
     $(document).ready(function() {
-        //si se hace click en un td que no tiene la clase comparar (porque tiene el checkbox)
+
+        //si se hace click en un td que no tiene la clase comparar (no se clicka en un checkbox)
         $("tr td").on("click", function(evento) {
             if (!$(this).hasClass('comparar')) {
-                //Te redirija a las estadísticas individuales
+                //Te redirija a las estadísticas individuales del jugador clickado
                 window.location.href = "<?= base_url('Usuario_c/estadisticas/') ?>" + $(this).parent().children(':first').html();
             }
         })
 
-        //Si se clicka el botón comparar recorremos todos los td y comprobaremos si está checkeado
-        $("#btn-comparar").on("click", function(evento) {
+        //Al intentar comparar jugadores
+        $("#compararJugadores").on("submit", function(evento) {
+            //Creamos un array
             let jugadores = [];
             $("tr td").each(function(index, elemento) {
                 if ($(elemento).hasClass('comparar') && $(elemento).children().is(':checked')) {
-                    //pillamos el username del jugador que está en un td:hidden en la misma fila y primera posición y lo guardamos en un array.
+                    //pillamos el username del jugador que está en un td:hidden en la misma fila y primera posición y lo guardamos.
                     jugadores.push($(elemento).parent().children(':first').html());
                 }
             });
 
-            if (jugadores.length > 1 && jugadores.length <= 4) {
-                console.log(jugadores);
-                window.location.href = "<?= base_url('Usuario_c/estadisticas/') ?>" + $(jugadores);
+            //Si se han seleccionado menos de 2 jugadores o más de 2 jugadores aparecerán distintos errores
+            if (jugadores.length < 0 || jugadores.length < 2 || jugadores.length == 3) {
+                evento.preventDefault();
+                if (jugadores.length == 0 || jugadores.length < 2) {
+                    $("#alerta-comparar").html("Tienes que comparar dos jugadores.");
+                    $("#alerta-comparar").slideDown(500);
+                } else {
+                    $("#alerta-comparar").html("Solo puedes comparar como máximo dos jugadores.");
+                    $("#alerta-comparar").slideDown(500);
+                }
+
+                //Quitamos error.
+                window.setTimeout(function() {
+                    $("#alerta-comparar").fadeTo(500, 0).slideUp(800, function() {
+                        $(this).css('opacity', '');
+                    });
+                }, 3000);
             }
-        });
+        })
     });
 </script>
 <style>
@@ -37,16 +53,22 @@
     td img {
         width: 100px;
     }
+
+    #alerta-comparar {
+        display: none;
+    }
 </style>
 <div class="row justify-content-center flex-start h-100 bg-white">
+    <div class="alert alert-danger mb-0" id="alerta-comparar" role="alert">
+    </div>
     <table class="col-12 table">
-        <form action="<?= base_url('Usuario_c/estadisticas/') ?>" method="POST">
+        <form action="<?= base_url('Usuario_c/estadisticas/') ?>" id="compararJugadores" method="POST">
             <tr>
                 <th>Foto</th>
                 <th>Apellidos y Nombre</th>
                 <th>Fecha de nacimiento</th>
                 <th>Equipo</th>
-                <th><input type="submit" class="btn btn-success" value="Comparar"></th>
+                <th><input type="submit" class="btn btn-success" value="Comparar" id="btn-comparar"></th>
             </tr>
             <?php
             //Mostramos los datos de cada jugador que está en la liga
